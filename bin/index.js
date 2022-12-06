e3e0d06ad0cafb4a46627b226549ed41958d767c
Elon Niu <elonniu@amazon.com>
2022-12-06 17:39:52 +0800

    update bin

--- bin/index.js
+++ bin/index.js
@@ -1,3 +1,53 @@
 #! /usr/bin/env node
 
-console.log("sst-gcr");
+import * as path from "path";
+import * as fs from "fs";
+
+if (!process.argv[2]) {
+    throw new Error("Must input aws partition");
+}
+
+const partition = process.argv[2];
+
+if (partition !== "aws" && partition !== 'aws-cn') {
+    throw new Error("The aws partition must be 'aws' or 'aws-cn'");
+}
+
+const walk = dir => {
+    try {
+        let results = [];
+        const list = fs.readdirSync(dir);
+        list.forEach(file => {
+            file = path.join(dir, file);
+            const stat = fs.statSync(file);
+            if (stat && stat.isDirectory()) {
+                // Recurse into subdir
+                results = [...results, ...walk(file)];
+            } else {
+                // Is a file
+                results.push(file);
+            }
+        });
+        return results;
+    } catch (error) {
+        console.error(`Error when walking dir ${dir}`);
+    }
+};
+const updatePartition = (partition, filePath) => {
+    if (!filePath.endsWith("ts") && !filePath.endsWith("mjs") && !filePath.endsWith("js")) {
+        return;
+    }
+    const old_partition = partition === 'aws' ? 'aws-cn' : 'aws';
+    const oldContent = fs.readFileSync(filePath, {encoding: 'utf8'});
+    const newContent = oldContent.replaceAll(
+        `arn:${old_partition}:`,
+        `arn:${partition}:`
+    );
+    if (oldContent.toString() !== newContent.toString()) {
+        fs.writeFileSync(filePath, newContent, {encoding: 'utf-8'});
+        console.log(`${filePath} Updated`);
+    }
+};
+
+const dir = `${process.env.PWD}/node_modules/@serverless-stack`;
+walk(dir).forEach(filePath => updatePartition(partition, filePath));
