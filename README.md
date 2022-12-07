2bb0346dd564eb7dcdb840cdb9fd82f122abc590
Elon Niu <elonniu@amazon.com>
2022-12-07 18:40:24 +0800

    upgrade version

--- README.md
+++ README.md
@@ -11,9 +11,5 @@ npm install aws-partition -g
 ## CLI
 
 ```bash
-aws-partition aws
-aws-partition aws-cn
-aws-partition aws-us-gov
-aws-partition aws-iso
-aws-partition aws-iso-b
+aws-partition
 ```
--- bin/index.mjs
+++ bin/index.mjs
@@ -3,24 +3,6 @@
 import * as path from "path";
 import * as fs from "fs";
 
-if (!process.argv[2]) {
-    throw new Error("Must input AWS partition");
-}
-
-const partition = process.argv[2];
-
-const partitions = [
-    'aws',
-    'aws-cn',
-    'aws-us-gov',
-    'aws-iso',
-    'aws-iso-b',
-];
-
-if (!partitions.includes(partition)) {
-    throw new Error(`The AWS partition must be in ${partitions.join(', ')}`);
-}
-
 const walk = dir => {
     let results = [];
 
@@ -45,7 +27,7 @@ const walk = dir => {
     return results;
 };
 
-const sst = (partition, file) => {
+const sst = (file) => {
 
     if (!file.endsWith("ts") && !file.endsWith("mjs") && !file.endsWith("js")) {
         return;
@@ -53,39 +35,14 @@ const sst = (partition, file) => {
 
     const oldContent = fs.readFileSync(file, {encoding: 'utf8'});
 
-    let newContent = oldContent.replaceAll(
-        /arn:(aws|aws-cn|aws-us-gov|aws-iso|aws-iso-b):ssm:\${app.region}/g,
-        'arn:${app.region.startsWith(\'cn\') ? \'aws-cn\' : \'aws\'}:ssm:${app.region}'
-    );
-
-    // newContent = newContent.replaceAll(
-    //     'arn:aws:execute-api:${region}',
-    //     'arn:${region.startsWith(\'cn\') ? \'aws-cn\' : \'aws\'}:execute-api:${region}'
-    // );
-
-    // newContent = newContent.replaceAll(
-    //     'arn:aws:appsync:${region}',
-    //     'arn:${region.startsWith(\'cn\') ? \'aws-cn\' : \'aws\'}:appsync:${region}'
-    // );
-    //
-    // newContent = newContent.replaceAll(
-    //     'arn:aws:s3:::${app.bootstrapAssets.bucketName}',
-    //     'arn:${app.region.startsWith(\'cn\') ? \'aws-cn\' : \'aws\'}:s3:::${app.bootstrapAssets.bucketName}'
-    // );
-
-    // newContent = newContent.replaceAll(
-    //     `"arn:aws:iam::aws:policy/service-role/AWSLambdaBasicExecutionRole"`,
-    //     "`arn:${this.node.root.region.startsWith('cn')?'aws-cn':'aws'}:iam::aws:policy/service-role/AWSLambdaBasicExecutionRole`"
-    // );
-
-    // *Pattern* : `^arn:(aws|aws-cn|aws-us-gov|aws-iso|aws-iso-b):ec2:[a-z\-0-9]*:[0-9]{12}:security-group/.*$`
-
-    newContent = newContent.replaceAll(
+    const newContent = oldContent.replaceAll(
+        '"arn:aws:iam::aws:policy/service-role/AWSLambdaBasicExecutionRole"',
+        "`arn:${process.env?.AWS_PARTITION || 'aws'}:iam::aws:policy/service-role/AWSLambdaBasicExecutionRole`"
+    ).replaceAll(
         /arn:(aws|aws-cn|aws-us-gov|aws-iso|aws-iso-b):/g,
-        `arn:${partition}:`
+        "arn:${process.env?.AWS_PARTITION || 'aws'}:"
     );
 
-    // arn:aws-cn:ssm:${app.region}
     if (oldContent.toString() !== newContent.toString()) {
         fs.writeFileSync(file, newContent, {encoding: 'utf-8'});
         console.info(`${file} Updated`);
@@ -94,8 +51,4 @@ const sst = (partition, file) => {
 
 // for sst
 walk(`${process.env.PWD}/node_modules/@serverless-stack`)
-    .forEach(file => sst(partition, file));
-
-// for other...
-
-// console.info(`AWS Partition has been switched to: ${partition}`);
+    .forEach(file => sst(file));
