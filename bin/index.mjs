12ffdefeeb42fb0cc9ab036bc5b58d1d1703bc05
Elon Niu <elonniu@amazon.com>
2022-12-07 20:24:33 +0800

    fixed tags

--- bin/index.mjs
+++ bin/index.mjs
@@ -2,6 +2,34 @@
 
 import * as path from "path";
 import * as fs from "fs";
+import execSh from "exec-sh";
+
+if (process.argv[2]) {
+    const command = process.argv[2];
+
+    if (command === 'update' || command === 'upgrade') {
+
+        execSh("npm i -g aws-partition --registry=https://registry.npmjs.org",
+            {cwd: "/home"},
+            function (err, out) {
+                if (err) {
+                    console.error(err.message);
+                }
+            });
+
+        process.exit(0);
+    }
+
+    if (command === 'version' || command === '-v') {
+        const json = JSON.parse(fs.readFileSync('../package.json', {encoding: 'utf8'}));
+        console.info(`aws-partition version: ${json.version}`);
+        process.exit(0);
+    }
+
+    console.info('unknown command');
+    process.exit(-1);
+}
+
 
 const walk = dir => {
     let results = [];
@@ -43,7 +71,7 @@ const sst = (file) => {
         "arn:${['aws-cn', 'aws-us-gov', 'aws-iso', 'aws-iso-b'].includes(process.env.AWS_PARTITION) ? process.env.AWS_PARTITION : 'aws'}:"
     ).replaceAll(
         'cdk.Tags.of(child).add("sst:',
-        '// cdk.Tags.of(child).add("sst:'
+        '// cdk-bak.Tags.of(child).add("sst:'
     );
 
     if (oldContent.toString() !== newContent.toString()) {
