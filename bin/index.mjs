62ff622a5e0e5c86671395c344ce6d420825d73a
Elon Niu <elonniu@amazon.com>
2022-12-10 14:04:12 +0800

    fixed tags

--- bin/index.mjs
+++ bin/index.mjs
@@ -8,30 +8,34 @@ const version = () => {
     console.info(`aws-partition version: 0.0.19`);
 };
 
-if (process.argv[2]) {
-    const command = process.argv[2];
+if (!process.argv[2]) {
+    console.info('Must input command');
+    process.exit(-1);
+}
 
-    if (command === 'update' || command === 'upgrade') {
+const command = process.argv[2];
 
-        execSh("npm i -g aws-partition --registry=https://registry.npmjs.org",
-            {},
-            function (err, out) {
-                if (err) {
-                    console.error(err.message);
-                }
-            });
+if (command === 'update' || command === 'upgrade') {
+    execSh("npm i -g aws-partition --registry=https://registry.npmjs.org",
+        {},
+        function (err, out) {
+            if (err) {
+                console.error(err.message);
+            }
+        });
+    version();
+    process.exit(0);
+}
 
-        version();
+if (command === 'version' || command === '-v') {
+    version();
+    process.exit(0);
+}
 
-        process.exit(0);
-    }
+const partitions = ['aws', 'aws-cn', 'aws-us-gov', 'aws-iso', 'aws-iso-b'];
 
-    if (command === 'version' || command === '-v') {
-        version();
-        process.exit(0);
-    }
-
-    console.info('unknown command');
+if (!partitions.includes(process.argv[2])) {
+    console.info('Partition must be one of: ' + partitions.join(', '));
     process.exit(-1);
 }
 
@@ -62,12 +66,20 @@ const sst = (file) => {
 
     const oldContent = fs.readFileSync(file, {encoding: 'utf8'});
 
+    // const newContent = oldContent.replaceAll(
+    //     '"arn:aws:iam::aws:policy/service-role/AWSLambdaBasicExecutionRole"',
+    //     "`arn:${['aws-cn', 'aws-us-gov', 'aws-iso', 'aws-iso-b'].includes(process.env.AWS_PARTITION) ? process.env.AWS_PARTITION : 'aws'}:iam::aws:policy/service-role/AWSLambdaBasicExecutionRole`"
+    // ).replaceAll(
+    //     /arn:(aws|aws-cn|aws-us-gov|aws-iso|aws-iso-b):/g,
+    //     "arn:${['aws-cn', 'aws-us-gov', 'aws-iso', 'aws-iso-b'].includes(process.env.AWS_PARTITION) ? process.env.AWS_PARTITION : 'aws'}:"
+    // ).replaceAll(
+    //     'cdk.Tags.of(child).add("sst:',
+    //     '// cdk-bak.Tags.of(child).add("sst:'
+    // );
+
     const newContent = oldContent.replaceAll(
-        '"arn:aws:iam::aws:policy/service-role/AWSLambdaBasicExecutionRole"',
-        "`arn:${['aws-cn', 'aws-us-gov', 'aws-iso', 'aws-iso-b'].includes(process.env.AWS_PARTITION) ? process.env.AWS_PARTITION : 'aws'}:iam::aws:policy/service-role/AWSLambdaBasicExecutionRole`"
-    ).replaceAll(
         /arn:(aws|aws-cn|aws-us-gov|aws-iso|aws-iso-b):/g,
-        "arn:${['aws-cn', 'aws-us-gov', 'aws-iso', 'aws-iso-b'].includes(process.env.AWS_PARTITION) ? process.env.AWS_PARTITION : 'aws'}:"
+        `arn:${command}:`
     ).replaceAll(
         'cdk.Tags.of(child).add("sst:',
         '// cdk-bak.Tags.of(child).add("sst:'
